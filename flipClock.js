623763116f5d09b84df274c5b18bffdf5235cced
Kevin Coleman <kevin.n.coleman@gmail.com>
2013-02-15 10:27:08 -0800

    CSS tranform test
    Trying out CSS transform properties for the flip animation.

--- flipClock.js
+++ flipClock.js
@@ -1,46 +1,50 @@
 $(document).ready(function(){
 
     setInterval(function() {
-    
 
         var time = new Date;
 
+            // Set first digit
+            var firstDigit = 0;
+            if ((time.getHours() > 9 && time.getHours() < 13) || (time.getHours() > 21 && time.getHours() < 24)) {
+                firstDigit = 1;
+            }
 
-        // Set & display first digit
-        var firstDigit = 0;
-        if ((time.getHours() > 9 && time.getHours() < 13) || (time.getHours() > 21 && time.getHours() < 24)) {
-            firstDigit = 1;
-        }
-        $('.hourTens').text(firstDigit);
-    
+            // Set second digit
+            var secondDigit = time.getHours();
+            if (secondDigit > 12) {
+                secondDigit -= 12;
+            }
+            if (secondDigit > 9) {
+                secondDigit -= 10;
+            }
 
-        // Set & display second digit
-        var secondDigit = time.getHours();
-        if (secondDigit > 12) {
-            secondDigit -= 12;
-        }
-        if (secondDigit > 9) {
-            secondDigit -= 10;
-        }
-        $('.hourOnes').text(secondDigit);
+            // Set third digit
+            var thirdDigit = time.getMinutes();
+            if (thirdDigit < 10) {
+                thirdDigit = 0;
+            } else {
+                thirdDigit = (thirdDigit + "")[0];
+            }
 
+            // Set fourth digit
+            var fourthDigit = time.getMinutes();
+            if (fourthDigit > 9) {
+                fourthDigit = (fourthDigit + "")[1];
+            }
 
-        // Set & display third digit
-        var thirdDigit = time.getMinutes();
-        if (thirdDigit < 10) {
-            thirdDigit = 0;
-        } else {
-            thirdDigit = (thirdDigit + "")[0];
-        }
-        $('.minuteTens').text(thirdDigit);        
+        // Animate changing tiles
+        //if ((time.getSeconds() - 1) == 58) {
 
+        //    $('div.digit').html('<div class="flipper">0</div>');
+        //} else {
 
-        // Set & display fourth digit
-        var fourthDigit = time.getMinutes();
-        if (fourthDigit > 9) {
-            fourthDigit = (fourthDigit + "")[1];
-        }
-        $('.minuteOnes').text(fourthDigit);
+            $('.hourTens').text(firstDigit);
+            $('.hourOnes').text(secondDigit);
+            $('.minuteTens').text(thirdDigit);        
+            $('.minuteOnes').text(fourthDigit);
+
+        //}
 
     }, 500);
 
